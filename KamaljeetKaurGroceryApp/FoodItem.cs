9fe2324996a8bbcb9258620cbec87376174ef850
Kamaljeet Kaur <jeetkamalkaur303@gmail.com>
2022-04-14 09:57:54 -0400

    chathub created

--- KamaljeetKaurGroceryApp/FoodItem.cs
+++
@@ -1,9 +0,0 @@
-public class FoodItem
-{
-    public string FoodName { get; set; }
-    public string ProductionCountry { get; set; }
-    public float Price { get; set; }
-    public string Color{get; set;}
-    public float Quantity { get; set; }
-
-}
